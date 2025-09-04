7caa365d08ddc581ede0e199fc55c6ade16f6d90
theabhipatel <theabhipatel.co@gmail.com>
2025-09-04 10:57:52 +0530

    chore: testing husky and prettier configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 const App = () => {
   return (
-    <div className="flex h-screen justify-center items-center flex-col">
-      <span className="bg-gradient-to-r from-orange-500 to-pink-500 bg-clip-text text-transparent text-3xl font-bold text-center">
+    <div className="flex h-screen flex-col items-center justify-center">
+      <span className="bg-gradient-to-r from-orange-500 to-pink-500 bg-clip-text text-center text-3xl font-bold text-transparent">
         Welcome to
       </span>
-      <h1 className="bg-gradient-to-r from-purple-500 to-pink-500 bg-clip-text text-transparent text-6xl font-bold text-center">
+
+      <h1 className="bg-gradient-to-r from-purple-500 to-pink-500 bg-clip-text text-center text-6xl font-bold text-transparent">
         TAP React Template
       </h1>
     </div>
