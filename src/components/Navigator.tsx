ed23fd28c77be04b99a2022d48f69ddc6062c8aa
theabhipatel <theabhipatel.co@gmail.com>
2025-09-05 13:30:16 +0530

    feat: add links in navigation for home and profile page

--- src/components/Navigator.tsx
+++ src/components/Navigator.tsx
@@ -1,5 +1,16 @@
+import { Link } from "react-router";
+
 const Navigator = () => {
-  return <div>Navigator</div>;
+  return (
+    <div className="mt-5 flex gap-3">
+      <Link to={"/"} className="font-semibold underline hover:text-blue-600">
+        Home
+      </Link>
+      <Link to={"/profile"} className="font-semibold underline hover:text-blue-600">
+        Profile
+      </Link>
+    </div>
+  );
 };
 
 export default Navigator;
