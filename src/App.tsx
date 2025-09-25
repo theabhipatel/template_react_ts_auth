2d24b0cb52adc9dd18378c839ee4d5ab5d78dd39
theabhipatel <theabhipatel.co@gmail.com>
2025-09-25 10:34:26 +0530

    chore: add store provider in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import { BrowserRouter } from "react-router";
 import AppRoutes from "./routes/AppRoutes";
+import { Provider } from "react-redux";
+import { store } from "./store/store";
 
 const App = () => {
   return (
     <BrowserRouter>
-      <AppRoutes />
+      <Provider store={store}>
+        <AppRoutes />
+      </Provider>
     </BrowserRouter>
   );
 };
