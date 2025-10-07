6702ca4c7becc6ac98251fb571a001b0203af5a0
theabhipatel <theabhipatel.co@gmail.com>
2025-10-07 10:31:36 +0530

    chore: add todo comment in private routes

--- src/hoc/PrivateRoutes.tsx
+++ src/hoc/PrivateRoutes.tsx
@@ -5,6 +5,7 @@ const PrivateRoutes = () => {
   const user = useAppSelector((state) => state.auth.user);
   const currentPath = window.location.pathname + window.location.search;
   const redirectTo = encodeURIComponent(currentPath);
+  // [TODO]: Have to check logic is proper working on not.
 
   return user ? <Outlet /> : <Navigate to={`/login?redirect=${redirectTo}`} />;
 };
